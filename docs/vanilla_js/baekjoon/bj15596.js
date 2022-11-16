31899945bf29e83c9cc4d53d4c18be18750217ec
seonbuzz <ddonsdevelop@gmail.com>
2022-11-16 16:02:34 +0900

    commit

--- docs/vanilla_js/baekjoon/bj15596.js
+++ docs/vanilla_js/baekjoon/bj15596.js
@@ -1,20 +1,20 @@
 //※수업 문제 ! 백준에는 js 제출 불가
-//Java는 long sum(int[] a); (클래스 이름:Test)
 
 //문제 : 정수 n개가 주어졌을 때, n개의 합을 구하는 함수를 작성하라
-//a : 합을 구해야 하는 정수 n개가 저장되어 있는 '배열' (0 ≤ a[i] ≤ 1,000,000, 1 ≤ n ≤ 3,000,000)
+//a : 합을 구해야 하는 정수 n개가 저장되어 있는 '배열'  (0 ≤ a[i] ≤ 1,000,000, 1 ≤ n ≤ 3,000,000)
 //리턴값 : a에 포함되어 있는 정수 n개의 합
 
-// param: 1
+// param : a
 function sum(a) {
-  let sum = 0;
+  let sum = 0; //변수 sum을 0으로 선언
   for (let number of a) {
-    sum += number;
+    // param a에 number 변수 선언
+    sum += number; // sum = sum+number
   }
-  return sum;
+  return sum; //a에 포함되어있는 정수 n개의 값 ↓
 }
 
 {
-  let arr = [1, 2, 3, 4, 5];
-  console.log(sum(arr));
+  let arr = [1, 2, 3, 4, 5]; //n개가 저장되어 있는 '배열' 그냥 지정함,,
+  console.log(sum(arr)); //sum(arr) 출력 !
 }
