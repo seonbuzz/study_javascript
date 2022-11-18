125e94b64a3a7f242df451fb934c020e352a1381
seonbuzz <ddonsdevelop@gmail.com>
2022-11-18 11:14:41 +0900

    js

--- docs/vanilla_js/function.js
+++ docs/vanilla_js/function.js
@@ -1,7 +1,7 @@
 /* function name(params1,params2,params3...) {
     code to be excuted
     return
-}*/
+          }*/
 
 //for문은 하나의 값을 스코프 안에다가 넣어주는 것
 
@@ -17,4 +17,46 @@ function sum(item_first, item_second) {
   sum_number = 6 + 8;
   let sum_function = sum(1, 2); // item_first = 1 , item_second = 2
   console.log();
+}
+
+//Normal Function
+function sum(item_first, item_second) {
+  return item_first + item_second;
+}
+
+//이름이 없는 function : anonymous Function(익명 함수), 잘 사용 안함.
+//이름을 빼버리고 변수에다가 담음.
+let calculateSum = function (item_first, item_second) {
+  return item_first + item_second;
+};
+//사용방법 : 걍 넣으면 됨. 이름을 변수에다가 담았을 뿐이지 변수에 param 담는 거하고 똑같음. function의 기능을 그대로 가지고 있음. 변수 이름 + param
+{
+  console.log(`${calculateSum(5, 7)}`);
+}
+
+// Arrow Function(js에서 가장 많이 사용하는 방식)  화살같은 기호를 사용해서 Function을 만듦.
+//js는 callback 기능을 많이 씀. 코드의 단순화를 위해 많이 씀..
+// =>  : anonymous function에서 function을 대치함.
+let calculateSumSecond = (item_first, item_second) => {
+  return item_first + item_second;
+};
+{
+  console.log(`${calculateSumSecond(7, 8)}`);
+}
+
+//rest parameters
+//Normal function
+// ... : 변수를 풀어헤치는것
+//가변적으로 움직일 수 있는 data type : array
+function printRestParam(...args) {
+  let restParam = (arg) => {
+
+    console.log(`arg : ${arg}`);
+  };
+  args.forEach(restParam); //forEach literal로 자꾸 풀어보자
+}
+
+{
+  printRestParam(2, 4, 5);
+  printRestParam(2, [4,3,2], 5, 6, 7);
 }
---
+++ docs/vanilla_js/toy/javascriptWithpoll.js
@@ -0,0 +1,108 @@
+const fs = require("fs");
+
+const filepath =
+  process.platform == "linux"
+    ? "/dev/stdin"
+    : "docs/vanilla_js/toy/poll_input.txt";
+
+let input = fs.readFileSync(filepath).toString().trim().split(" ").map(Number);
+
+//questions - object in Array
+const questions = [
+  {
+    questions_num: "Q1",
+    question: "해당 매장을 방문시 매장은 청결 하였습니까?",
+  },
+  {
+    questions_num: "Q2",
+    question: "주문시 직원은 고객님께 친절 하였습니까?",
+  },
+  {
+    questions_num: "Q3",
+    question: "주문하신 음료가 나오기까지 시간이 적당하였습니까?",
+  },
+  {
+    questions_num: "Q4",
+    question: "해당 매장을 다음에도 재방문 하실 의향이 있으십니까?",
+  },
+  {
+    questions_num: "Q5",
+    question: "직원이 제조한 음료에 대해 맛은 좋았습니까?",
+  },
+];
+
+// answer of questions - object in Array
+const answers = [
+  { answer_num: "A1", answer: "1.전혀 아니다" },
+  { answer_num: "A2", answer: "2.아니다" },
+  { answer_num: "A3", answer: "3.보통이다" },
+  { answer_num: "A4", answer: "4.그렇다" },
+  { answer_num: "A5", answer: "5.매우 그렇다" },
+];
+
+//input - 1번 Line에 적음.
+
+// Function  forEach문 활용해보기 ...못활용함
+//처리 - questions, answer of questions, input matching
+
+//questions
+function printQ(i) {
+  console.log(`${questions[i].questions_num}.${questions[i].question}`);
+}
+
+//answer of questions 왜 이게 출력이 안되지??param이 안쓰였음-->다른 방법 시도 해결해보기
+// function printA(i) {
+//   if (questions.questions_num == "Q1") {
+//     for (let i = 0; i < 3; i++) {
+//       console.log(`${answers[i].answer}`);
+//     }
+//   } else if (questions.questions_num == "Q2") {
+//     for (let i = 0; i < 4; i++) {
+//       console.log(`${answers[i].answer}`);
+//     }
+//   } else if (questions.questions_num == "Q3") {
+//     for (let i = 0; i < 2; i++) {
+//       console.log(`${answers[i].answer}`);
+//     }
+//   } else if (questions.questions_num == "Q4") {
+//     for (let i = 0; i < 5; i++) {
+//       console.log(`${answers[i].answer}`);
+//     }
+//   } else if (questions.questions_num == "Q5") {
+//     for (let i = 0; i < 3; i++) {
+//       console.log(`${answers[i].answer}`);
+//     }
+//   }
+// }
+//
+// function printA(i) {
+//   if (questions.questions_num == "Q1") {
+//     q_answer(0, 1, 2);
+//   } else if (questions.questions_num == "Q2") {
+//     q_answer(0, 1, 2);
+//   } else if (questions.questions_num == "Q3") {
+//     q_answer(0, 1, 2);
+//   } else if (questions.questions_num == "Q4") {
+//     q_answer(0, 1, 2);
+//   } else if (questions.questions_num == "Q5") {
+//     q_answer(0, 1, 2);
+//   }
+// }
+
+// function q_answer(...args) {
+//   let restParam = (arg) => console.log(`${answers.answer}`);
+//   args.forEach(q_answer);
+// }
+
+// input matching
+for (let i = 0; i < questions.length; i++) {
+  printQ(i);
+  printA(i);
+  console.log(`답) ${input[i]}`);
+}
+
+// questions.forEach(() => console.log(answer1));
+
+// for (let key of questions) {
+//   console.log(`${key} : ${question[key]}`);
+// }
