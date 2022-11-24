5a2b0372846acde17ad6e4d1b62974fc97627d3b
seonbuzz <ddonsdevelop@gmail.com>
2022-11-25 08:42:56 +0900

    commit

--- docs/vanilla_js/toy/jsWithInstructor2.js
+++ docs/vanilla_js/toy/jsWithInstructor2.js
@@ -5,6 +5,8 @@
 -process부터 적기 
 process를 명확히 할것*/
 
+//3차 Object in Array 방식 !
+
 const questions_list = [
   {
     question: "해당 매장을 방문시 매장은 청결 하였습니까?",
@@ -65,44 +67,75 @@ const questions_answers = [
 
 //예상 묶음 데이터
 /*[
-  [Q1, E1, E2, E3]  ->{questions_uid:Q1, answer_uids:[E1,E2], answer:}
-[Q2, E1, E2, E3, E4] ->{questions_uid:Q2, answers_uids:[E1, E2, E3, E4]}
-[Q3, E1, E2]->{questions_uid:Q3, answers_uids:[E1, E2]}
-[Q4, E1, E2, E3, E4, E5]->{questions_uid:Q4, answers_uids:[E1, E2, E3, E4, E5]]}
-[Q5, E1, E2, E3]->{questions_uid:Q5, answers_uids:[E1, E2, E3]}
-];*/
+    [Q1, E1, E2, E3]  ->{questions_uid:Q1, answer_uids:[E1,E2], answer:}
+  [Q2, E1, E2, E3, E4] ->{questions_uid:Q2, answers_uids:[E1, E2, E3, E4]}
+  [Q3, E1, E2]->{questions_uid:Q3, answers_uids:[E1, E2]}
+  [Q4, E1, E2, E3, E4, E5]->{questions_uid:Q4, answers_uids:[E1, E2, E3, E4, E5]]}
+  [Q5, E1, E2, E3]->{questions_uid:Q5, answers_uids:[E1, E2, E3]}
+  ];*/
 
 //1차 방식 : [Q1, Q2, Q3, Q4, Q5]
 //2차 방식 : Array in Array
 //3차 방식 : Object in Array  <- 이렇게 하는 것을 추천[{questions_uid:Q1, answer_uids:[E1,E2]}]
 
+//3차 방식 적용 방법
+
 let polls = []; //전체 묶음
 let question_compare;
-let questions = []; //내부 묶음
+let questions = {}; // 내부 묶음
+let answer_uids = []; // 내부 설문 답변 묶음
 for (let idx = 0; idx < questions_answers.length; idx++) {
   if (question_compare != questions_answers[idx]["questions_uid"]) {
-    if (questions.length > 0) {
+    if (Object.keys(questions).length > 0) {
+      questions["answer_uids"] = answer_uids;
       polls.push(questions);
-      questions = [];
+      questions = {};
+      answer_uids = [];
     }
-    // console.log(`${questions_answers[idx]["questions_uid"]}`);
-    // console.log(`${questions_answers[idx]["answer_uid"]}`);
-    questions.push(questions_answers[idx]["questions_uid"]);
-    questions.push(questions_answers[idx]["answer_uid"]);
+
+    // console.log(`!= : ${questions_answers[idx]["questions_uid"]}`);
+    // console.log(`!= : ${questions_answers[idx]["answer_uid"]}`);
+    questions["questions_uid"] = questions_answers[idx]["questions_uid"];
+    answer_uids.push(questions_answers[idx]["answer_uid"]);
   } else {
-    // console.log(`${questions_answers[idx]["answer_uid"]}`);
-    questions.push(questions_answers[idx]["answer_uid"]);
+    // console.log(`== : ${questions_answers[idx]["answer_uid"]}`);
+    answer_uids.push(questions_answers[idx]["answer_uid"]);
+    if (idx + 1 >= questions_answers.length) {
+      questions["answer_uids"] = answer_uids;
+      polls.push(questions);
+    }
   }
-  if (idx + 1 == questions_answers.length) {
-    polls.push(questions);
-  }
-  question_compare = questions_answers[idx]["questions_uid"]; //이전 uid 입력
+  question_compare = questions_answers[idx]["questions_uid"]; // 이전 uid 입력
 }
 
-console.log(`${polls}`);
+// console.log(`${polls}`);
 
 //Q5가 나오는 방법 두가지
 //1. if문을 쓰고 안에 push 작성
 //2. if문 안쓰고 마지막에 push만 작성
 
 //출력 : key값은 쪼개지말기 ~ (ex: 'Q'+idx )
+//polls[0]['questions_uid]
+//polls[0]['answer_uid'][0]
+//polls[0]['answer_uid'][1] ...
+
+//설문 문항을 가져오는 function
+
+function getQuestionByUid(question_uid) {
+  let question_desc;
+  for (question_list of questions_list) {
+    if (question_list["question_uid"] == question_uid) {
+      question_desc = question_list["question"];
+    }
+  }
+
+  return question_desc;
+}
+for (poll of polls) {
+  console.log(`${poll["questions_uid"]}`); // ==polls[idx]
+  let answer_uids = poll["answer_uids"];
+  answer_uids.forEach((answer_uid, index) => {
+    //answers
+    console.log(`${index + 1}.${answer_uid}`);
+  });
+}
